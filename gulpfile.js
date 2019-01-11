2c768dbcf6d4beab5c52b2b3a4e1d4a8a968d5db
VChet <17050347+VChet@users.noreply.github.com>
2019-01-11 22:06:31 +0500

    Replace gulp scripts with npm scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,15 @@
-const gulp = require('gulp');
-const stylelint = require('gulp-stylelint');
-const exec = require('child_process').exec;
+const gulp = require("gulp");
+const stylelint = require("gulp-stylelint");
 
-const usercss = "coinkeeper-dark.user.css";
-
-function lint() {
-  return gulp.src(
-    [
-      './**/*.css',
-      '!./node_modules/**'
-    ])
-    .pipe(stylelint({
-      reporters: [{
-        formatter: "string",
-        console: true
-      }]
-    }));
-}
-
-gulp.task('lint', function() {
-  return lint();
-});
-
-gulp.task('patch', function() {
-  lint();
-  return exec(`npx ver patch -p ${usercss}`);
-});
-
-gulp.task('minor', function() {
-  lint();
-  return exec(`npx ver minor -p ${usercss}`);
-});
-
-gulp.task('major', function() {
-  lint();
-  return exec(`npx ver major -p ${usercss}`);
+gulp.task("lint", function() {
+  return gulp.src([
+    "./**/*.css",
+    "!./node_modules/**"
+  ]).pipe(stylelint({
+    fix: true,
+    reporters: [{
+      formatter: "string",
+      console: true
+    }]
+  })).pipe(gulp.dest((file => file.base)));
 });
