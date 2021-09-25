46f04663db4b3b270afb87a8acd6a076cdfebd63
VChet <17050347+VChet@users.noreply.github.com>
2021-09-25 14:28:57 +0500

    fix(mappings): add new color mapping, color popup text

--- tools/generate.js
+++ tools/generate.js
@@ -18,6 +18,7 @@ const mappings = {
   "$background: #dedcdc": "var(--gray-light)",
   // Color
   "color: #333": "color: var(--text)",
+  "color: #5f6160": "color: var(--text)",
   "color: #64635f": "color: var(--text)",
   "color: #696868": "color: var(--text)",
   "color: #757474": "color: var(--text)",
