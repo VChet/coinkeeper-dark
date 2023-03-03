637dfe395e4c184c59d9919103a98cc107d3b78d
VChet <17050347+VChet@users.noreply.github.com>
2023-03-03 23:13:24 +0600

    chore: add bump script

---
+++ bump.cjs
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const files = fs.readdirSync(__dirname).filter(filename => filename.endsWith('.user.css'));
+
+
+function main () {
+  files.forEach(filename => {
+    const path = `${__dirname}/${filename}`;
+    const contents = fs.readFileSync(path, 'utf8');
+    const updatedContents = contents.replace(/\b\d+\.\d+\.\d+\b/g, process.env.npm_package_version);
+    fs.writeFileSync(path, updatedContents, 'utf8');
+  });
+
+  execSync(`git add ${files.join(' ')}`).toString();
+}
+
+main()
