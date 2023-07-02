9591e9bb8cb56386b21d90e8cfdf906fd3fafe35
VChet <17050347+VChet@users.noreply.github.com>
2023-07-02 17:20:38 +0400

    refactor(bump): destruct import

--- bump.cjs
+++ bump.cjs
@@ -1,13 +1,13 @@
-const fs = require('fs');
+const { readdirSync, readFileSync, writeFileSync } = require('fs');
 const { execSync } = require('child_process');
 
 function main () {
-  const files = fs.readdirSync(__dirname).filter(filename => filename.endsWith('.user.css'));
+  const files = readdirSync(__dirname).filter(filename => filename.endsWith('.user.css'));
   files.forEach(filename => {
     const path = `${__dirname}/${filename}`;
-    const contents = fs.readFileSync(path, 'utf8');
+    const contents = readFileSync(path, 'utf8');
     const updatedContents = contents.replace(/\b\d+\.\d+\.\d+\b/g, process.env.npm_package_version);
-    fs.writeFileSync(path, updatedContents, 'utf8');
+    writeFileSync(path, updatedContents, 'utf8');
   });
 
   execSync(`git add ${files.join(' ')}`).toString();
